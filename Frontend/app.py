83f289d7e055f7e5f5f20599e8e6449b6e3a10d0
Nithish77 <nithishgangineni07@gmail.com>
2026-01-22 12:38:01 +0530

    Updated Frontend\app.py  for deployment on render

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,37 +1,41 @@
 import streamlit as st
-import requests
+import numpy as np
+import joblib
+import os
 
+# -------------------------------
+# Load Model (UPDATED PATH)
+# -------------------------------
+MODEL_PATH = os.path.join(
+    "Backend", "iris_random_forest_model.pkl"
+)
+
+model = joblib.load(MODEL_PATH)
+
+# -------------------------------
+# UI
+# -------------------------------
 st.title("🌸 IRIS FLOWER SPECIES PREDICTION")
 st.write("Please enter the details to get the flower species")
 
-# Input fields
+# -------------------------------
+# Input Sliders
+# -------------------------------
 sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.4)
 sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.4)
 petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.3)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
 
+# -------------------------------
+# Prediction
+# -------------------------------
 if st.button("Predict"):
 
-    # JSON data to send to Flask API
-    input_data = {
-        "sepal_length": sepal_length,
-        "sepal_width": sepal_width,
-        "petal_length": petal_length,
-        "petal_width": petal_width
-    }
+    input_data = np.array([
+        [sepal_length, sepal_width, petal_length, petal_width]
+    ])
 
-    # Call Flask backend
-    response = requests.post(
-        "http://127.0.0.1:5000/predict",
-        json=input_data
-    )
+    prediction = model.predict(input_data)[0]
 
-    if response.status_code == 200:
-        result = response.json()
-        prediction = result["predicted_species"]
-      
-
-        st.success(f"🌼 Predicted Species: **{prediction}**")
-        
-    else:
-        st.error("❌ Error in prediction. Is the backend running?")
+    # Model already returns label like "setosa"
+    st.success(f"🌼 Predicted Species: **Iris-{prediction.capitalize()}**")
